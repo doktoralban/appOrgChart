5e0b491b6d2a7ccd20a7ab749e766f6ec215ac07
Aydın Türker <doktoralban@outlook.com>
2021-06-27 21:00:48 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxOrgChartModule } from 'ngx-org-chart';
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    NgxOrgChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
